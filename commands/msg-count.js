d86b43b0ebee8c2f9ab129b89ab81664af3b3e60
RetroCraft <retrocraft314@gmail.com>
2018-05-10 19:48:52 -0400

    fix(msgcount): Error handling

--- commands/msg-count.js
+++ commands/msg-count.js
@@ -13,26 +13,28 @@ module.exports = (args, msg) => {
     }
     if (referenced) channel = referenced;
   }
-  channel.fetchMessages({ limit: 100 }).then((messages) => {
-    console.log(`[msgcount] found ${messages.size} messages for #${channel.name}`);
-    const stats = {};
-    messages.forEach((message) => {
-      let name = message.author.username;
-      if (message.member) name = message.member.nickname;
-      if (stats[name]) {
-        stats[name] += 1;
-      } else {
-        stats[name] = 1;
-      }
-    });
-    const sorted = _.sortBy(
-      _.map(_.keys(stats), name => ({ name, num: stats[name] })),
-      'num',
-    ).reverse();
-    const body = sorted.map(user => `**${user.name}**: ${user.num}`).join('\n');
-    msg.channel.send(new Discord.RichEmbed()
-      .setAuthor('Channel Stats')
-      .setTitle(`User activity of the last 100 messages in #${channel.name}:`)
-      .setDescription(body));
-  });
+  channel
+    .fetchMessages({ limit: 100 })
+    .then((messages) => {
+      const stats = {};
+      messages.forEach((message) => {
+        let name = message.author.username;
+        if (message.member) name = message.member.nickname;
+        if (stats[name]) {
+          stats[name] += 1;
+        } else {
+          stats[name] = 1;
+        }
+      });
+      const sorted = _.sortBy(
+        _.map(_.keys(stats), name => ({ name, num: stats[name] })),
+        'num',
+      ).reverse();
+      const body = sorted.map(user => `**${user.name}**: ${user.num}`).join('\n');
+      msg.channel.send(new Discord.RichEmbed()
+        .setAuthor('Channel Stats')
+        .setTitle(`User activity of the last 100 messages in #${channel.name}:`)
+        .setDescription(body));
+    })
+    .catch(() => console.error(`[msgcount] failed to get messages for ${channel.name}`));
 };
