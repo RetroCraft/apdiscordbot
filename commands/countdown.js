62aef6a698b8721df642ac00a89709932b6d1bd1
RetroCraft <retrocraft314@gmail.com>
2018-05-10 23:02:25 -0400

    feat(countdown): Parse time + cancel

--- commands/countdown.js
+++ commands/countdown.js
@@ -1,24 +1,50 @@
-module.exports = (args, msg) => {
-  const time = +args[1] * 1000;
-  const timeStart = new Date().getTime();
-  const timeEnd = timeStart + time;
-  if (!time) {
-    msg.channel.send("I don't understand that time, sorry");
-    return;
+const moment = require('moment');
+
+const runningCountdowns = {};
+
+const parseTime = (time) => {
+  if (+time) {
+    const timeStart = Date.now();
+    const duration = time * 1000;
+    return timeStart + duration;
+  } else if (moment(time, 'HH:mm:ss').isValid()) {
+    return +moment(time, 'HH:mm:ss');
   }
-  let timerMessage;
-  msg.channel.send(`**Countdown:** ${Math.floor(time / 1000)}`).then((message) => {
-    timerMessage = message;
-  });
-  const repeat = setInterval(() => {
-    const now = new Date().getTime();
-    if (now < timeEnd) {
-      const timeLeft = timeEnd - now;
-      timerMessage.edit(`**Countdown:** ${Math.ceil(timeLeft / 1000)}`);
+  return null;
+};
+
+module.exports = (args, msg) => {
+  if (args[1] === 'cancel') {
+    if (runningCountdowns[`${msg.author}${msg.channel}`]) {
+      clearInterval(runningCountdowns[`${msg.author}${msg.channel}`]);
+      delete runningCountdowns[`${msg.author}${msg.channel}`];
+      msg.channel.send('Countdown cancelled');
     } else {
-      clearInterval(repeat);
-      timerMessage.delete();
-      msg.channel.send(`${msg.author} your countdown has finished!`);
+      msg.channel.send("You don't have any active countdowns in this channel...");
     }
-  }, 2000);
+  } else {
+    let timerMessage;
+    const timeEnd = parseTime(args[1]);
+    if (!timeEnd) {
+      msg.channel.send("I don't understand that time...try a number of seconds (i.e. 10) or a time (i.e. 12:30)");
+      return;
+    }
+    msg.channel
+      .send(`**Countdown:** ${Math.floor((timeEnd - Date.now()) / 1000)}`)
+      .then((message) => {
+        timerMessage = message;
+      });
+    const repeat = setInterval(() => {
+      const now = Date.now();
+      if (now < timeEnd) {
+        const timeLeft = timeEnd - now;
+        timerMessage.edit(`**Countdown:** ${Math.ceil(timeLeft / 1000)}`);
+      } else {
+        clearInterval(repeat);
+        timerMessage.delete();
+        msg.channel.send(`${msg.author} your countdown has finished!`);
+      }
+    }, 2000);
+    runningCountdowns[`${msg.author}${msg.channel}`] = repeat;
+  }
 };
