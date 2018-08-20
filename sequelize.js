9c7dcfe480d639bbcc055c0e06c0878f110049ad
RetroCraft <retrocraft314@gmail.com>
2018-08-20 13:27:44 -0400

    fix(sequelize): disable logging

--- sequelize.js
+++ sequelize.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: { ssl: true },
+  logging: process.env.NODE_ENV !== 'production' ? console.log : false,
 });
 
 module.exports.db = db;
